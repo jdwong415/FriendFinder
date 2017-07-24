738c3f28282d7771e705bdf3f0d2344af17cbc9c
Jonathan Wong <jdwong2@yahoo.com>
2017-07-23 23:31:38 -0700

    Change score values

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,11 +7,11 @@ var friends = [
   }, {
     name: "Elmo",
     photo: "https://pbs.twimg.com/profile_images/716986458406424576/8AOacOOQ.jpg",
-    scores: [5, 3, 5, 1, 5, 3, 5, 1, 5, 3]
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
   }, {
     name: "Pumba",
     photo: "https://pbs.twimg.com/profile_images/743040796056649730/1Bd3DOFW.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
   }, {
     name: "Batman",
     photo: "https://s-media-cache-ak0.pinimg.com/736x/fd/e1/ab/fde1abf2331273092aa0191ce3a7c03f--cartoon-batman-batman-comic.jpg",
@@ -19,7 +19,7 @@ var friends = [
   }, {
     name: "Mickey Mouse",
     photo: "https://s-media-cache-ak0.pinimg.com/736x/a7/64/2e/a7642e7f254404405915f19323418e39--disney-pics-disney-fan.jpg",
-    scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 4]
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
   }
 ];
 
